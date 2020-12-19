5b02e29c198ba86fa9aaebda50954d003f11f01d
krnsk0 <jkurinsky@gmail.com>
2020-12-19 16:34:06 -0500

    complete observable on process close

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { spawn } from 'child_process';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
 
 const progress = spawn('./build/progress', ['1000', '20']);
 
 fromEvent(progress.stdout, 'data')
-  .pipe(map((data: any) => data.toString().trim()))
-  .subscribe((x) => console.log(x));
+  .pipe(
+    map((data: any) => data.toString().trim()),
+    takeUntil(fromEvent(progress, 'close'))
+  )
+  .subscribe({
+    next: (data: string) => console.log(data),
+    error: (error: string) => console.log(error),
+    complete: () => console.log('done'),
+  });
